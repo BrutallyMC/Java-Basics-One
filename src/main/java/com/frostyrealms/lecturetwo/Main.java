5f71f30dd40e1400a52ef8ef8ded2099966a38e1
Luke (Brutally) <147535454+BrutallyMC@users.noreply.github.com>
2024-03-03 18:22:11 -0500

    Completed the Java Basics #1 course!

---
+++ src/main/java/com/frostyrealms/lecturetwo/Main.java
@@ -0,0 +1,13 @@
+package com.frostyrealms.lecturetwo;
+
+// JAVA BASICS LECTURE TWO
+// Understanding further functionality.
+//
+// What will be covered:
+// - If Statements and Operators
+// - Null
+// - Exceptions
+// - Classes, Instances and Constructors
+
+public class Main {
+}
