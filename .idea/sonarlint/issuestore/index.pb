73de3514845828986a822b97d7c20bd545bb8987
Luke (Brutally) <147535454+BrutallyMC@users.noreply.github.com>
2024-03-03 22:54:56 -0500

    If statements and operators

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,7 +5,5 @@ c
 3src/main/java/com/frostyrealms/lectureone/Main.java,2/2/221b0de1095d585299d7035525a49d6b45d288e8
 \
 ,.idea/inspectionProfiles/Project_Default.xml,4/9/496a238a6afa168dbaf6efd37bb459331589579c
-d
-4src/main/java/com/frostyrealms/lecturetwo/Class.java,7/b/7b0a6e0f380ac0531e1b1aebc17412fde4e00e58
 c
 3src/main/java/com/frostyrealms/lecturetwo/Main.java,9/d/9da06af2f0e4444d478cbe2bf069f847331ed06f
--- src/main/java/com/frostyrealms/lecturetwo/Main.java
+++ src/main/java/com/frostyrealms/lecturetwo/Main.java
@@ -10,4 +10,88 @@ package com.frostyrealms.lecturetwo;
 // - Classes, Instances and Constructors
 
 public class Main {
+
+   public static void main(String[] args) {
+
+       // Creating an integer for demonstration.
+       int number = 5;
+
+       // IF STATEMENTS AND OPERATORS
+       //
+       // In Java there are what we call decision-making
+       // statements; one of the most common one of these
+       // statements would be an "if" statement. Below is
+       // going to be an example of an if statement:
+
+       // Note: Whatever is inside the parentheses is the
+       // condition which MUST be true in order for the
+       // code inside the curley brackets to run.
+       // At the end of the curley brackets you can add
+       // what we call an "else" statement. This is what
+       // is run when the conditions of the if statement
+       // are not met.
+
+       // If statement example number 1
+       if (number > 3) {
+           // This would return true because 5 is greater
+           // than 3.
+       } else {
+           // This code would not run because the if
+           // statement already returned true, however,
+           // if it were to return false, then this block
+           // of code would run.
+       }
+
+       // If statement example number 2
+       if (number < 3) {
+           // This would return false because 5 is not
+           // less than 3.
+       } else {
+           // This code would run because the if statement
+           // returned false.
+       }
+
+       // You can also combine both if and else statements.
+       // For example:
+       if (number > 3) {
+           // In this case this code would run because 5
+           // is greater than 3.
+       } else if (number < 2) {
+           // This code would not run at all because the
+           // initial if statement returned true.
+       }
+
+       // Note: If else statements can be infinite. Also,
+       // technically, a boolean is a form of "if" statement.
+       // For example:
+
+       boolean testBoolean = true;
+
+       if (testBoolean == true) {
+           // Since the boolean variable that we have created
+           // is defined as true, this code would run. Also
+           // note: here we are using "==" to compare the two
+           //
+       }
+
+       // Seeing that our variable that we are using in this
+       // if statement is a boolean we can actually cut out
+       // the "=="s and any type of comparison at all. Example:
+       if (testBoolean) {
+           // This code would run since our "testBoolean"
+           // variable is set to true.
+       }
+
+       // TYPES OF OPERATORS
+       //
+       // ">" GREATER THAN
+       // "<" LESS THAN
+       // ">=" GREATER THAN OR EQUAL TO
+       // "<=" LESS THAN OR EQUAL TO
+       // "==" EQUAL TO
+       // "!=" NOT EQUAL TO
+       // "&&" AND
+       // "||" OR
+
+   }
 }
